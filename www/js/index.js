a2ebba4a18b3e379df26812de4a3acfa1995cb11
father <459633095@qq.com>
2016-11-11 16:44:39 +0800

    微信公众号

--- www/js/index.js
+++ www/js/index.js
@@ -262,7 +262,44 @@
 			swiperAnimate.swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
 		}
 	})
-
+	$.post("http://459633095.applinzi.com/php/getsign.php", {
+			url: window.location.href
+		}, function(data) {
+			pos = data.indexOf('}');
+			dataStr = data.substring(0, pos + 1);
+			objData = JSON.parse(dataStr);
+			wx.config({
+				debug: true,
+				appId: objData.appId,
+				timestamp: objData.timestamp,
+				nonceStr: objData.nonceStr,
+				signature: objData.signature,
+				jsApiList: [
+					'chooseImage','scanQRCode'
+				]
+			})
+		})
+		wx.ready(function() {
+			$('#bt_xc').tap(function() {
+				wx.chooseImage({
+					count: 1, // 默认9
+					sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+					sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+					success: function(res) {
+						var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+					}
+				});
+			})
+	    	$('#bt_ewm').tap(function() {
+	                wx.scanQRCode({
+	                needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+	                scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+	                success: function (res) {
+	                var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+	            }
+	            });
+	         })   
+		});
 
 
 /***/ },
@@ -324,7 +361,7 @@
 
 	    function g(a, b, c) {
 	        var d, e, f;
-	        switch (delete b.err_code, delete b.err_desc, delete b.err_detail, d = b.errMsg, d || (d = b.err_msg, delete b.err_msg, d = h(a, d, c), b.errMsg = d), c = c || {}, c._complete && (c._complete(b), delete c._complete), d = b.errMsg || "", z.debug && !c.isInnerInvoke && alert(JSON.stringify(b)), e = d.indexOf(":"), f = d.substring(e + 1)) {
+	        switch (delete b.err_code, delete b.err_desc, delete b.err_detail, d = b.errMsg, d || (d = b.err_msg, delete b.err_msg, d = h(a, d, c), b.errMsg = d), c = c || {}, c._complete && (c._complete(b), delete c._complete), d = b.errMsg || "", z.debug && !c.isInnerInvoke, e = d.indexOf(":"), f = d.substring(e + 1)) {
 	            case"ok":
 	                c.success && c.success(b);
 	                break;
